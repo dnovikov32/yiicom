1f3e338822576bca67fc3b6bf7ed4056ea557424
dn <dn@t1000.su>
2019-11-26 14:01:18 +0300

    feat(backend): removed old unnecessary widgets

--- src/backend/widgets/views/alerts.php
+++
@@ -1,7 +0,0 @@
-<div class="alerts">
-	<?php foreach($alerts as $key => $message) :?>
-		<div class="alert alert-<?php echo $key; ?> bg-<?php echo $key; ?>">
-			<?php echo $message; ?>
-		</div>
-	<?php endforeach;?>
-</div>
