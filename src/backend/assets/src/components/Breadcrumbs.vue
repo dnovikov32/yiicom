ad970440d03233cf754df6a685094cbb5306965c
dn <dn@t1000.su>
2019-11-28 14:01:56 +0300

    fix(admin): display breadcrumb after page refresh

--- src/backend/assets/src/components/Breadcrumbs.vue
+++ src/backend/assets/src/components/Breadcrumbs.vue
@@ -25,16 +25,26 @@
             }
         },
 
+        created: function () {
+            this.initItems();
+        },
+
         watch: {
             '$route' () {
+                this.initItems();
+            }
+        },
+
+        methods: {
+
+            initItems () {
                 this.items = this.$route.meta.breadcrumbs;
 
                 if (this.items && this.home && this.home.text !== this.items[0].text) {
                     this.items.splice(0, 0, this.home);
                 }
-
-                return this.items;
             }
+
         }
 
     }
