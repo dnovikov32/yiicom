b5a45bfdb81ff68210927ec256d92f58a33a6864
dn <dn@t1000.su>
2019-12-20 13:22:24 +0300

    feat(admin): added TitleField and Debug components

---
+++ src/backend/assets/src/components/Debug.vue
@@ -0,0 +1,34 @@
+<template>
+
+    <div v-if="isDev" class="yc-debug mt-3">
+        <a class="btn btn-secondary btn-xs mb-2" href="#yc-debug" data-toggle="collapse">Отладка</a>
+
+        <div class="collapse" id="yc-debug">
+            <div class="card card-body">
+                <pre>model: {{  model }}</pre>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+
+        props: {
+            model: {
+                type: Object,
+                default: function () {
+                    return {};
+                }
+            }
+        },
+
+        computed: {
+            isDev: function () {
+                return this.$store.getters['commerce/isDev'];
+            }
+        }
+
+    }
+</script>
---
+++ src/backend/assets/src/components/form/TitleField.vue
@@ -0,0 +1,69 @@
+<template>
+    <b-form-group
+        label="Заголовок"
+        label-for="title"
+        label-cols-sm="2"
+        description="Заголовок страница h1. Если пусто, то используется поле Название"
+    >
+        <b-form-input
+            id="title"
+            type="text"
+            v-model="model.title"
+            trim />
+    </b-form-group>
+</template>
+
+<script>
+    export default {
+        props: {
+            model: {
+                type: Object,
+                default: function () {
+                    return {};
+                }
+            }
+        },
+
+        created: function () {
+            this.initEvent(true);
+        },
+
+        watch: {
+            'model.id' () {
+                this.initEvent(false);
+            }
+        },
+
+        methods: {
+            initEvent (isComponentCreation) {
+                let self = this;
+
+                // TODO: try to remake to Vue
+                $(function (isComponentCreation) {
+                    if (self.model.id || self.model.title) {
+                        return false;
+                    }
+
+                    let $name = $('input[id="name"]');
+
+                    $name.on('keyup', function() {
+                        self.model.title = self.model.name;
+                    });
+
+                    if (isComponentCreation) {
+                        $name.on('focus', function () {
+                            if (self.model.title) {
+                                $name.unbind('keyup');
+                            }
+                        });
+
+                        $name.on('blur', function () {
+                            $name.unbind('keyup');
+                        });
+                    }
+                });
+            }
+        }
+
+    }
+</script>
