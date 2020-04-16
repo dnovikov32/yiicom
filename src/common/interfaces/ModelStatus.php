7a04870916d600c82c112f97cabc7f8ff4a1e020
dn <dn@t1000.su>
2020-04-16 13:33:06 +0300

    feat(ModelStatus): added statuses form schedule tasks

--- src/common/interfaces/ModelStatus.php
+++ src/common/interfaces/ModelStatus.php
@@ -6,17 +6,20 @@ interface ModelStatus
 {
     const STATUS_INACTIVE = 0;
     const STATUS_ACTIVE = 1;
-    const STATUS_SOFT_DELETE = 8;
+    const STATUS_SOFT_DELETE = 5;
+
+    const STATUS_PROCESS = 7;
+    const STATUS_COMPLETE = 8;
+    const STATUS_ERROR = 9;
 
     /**
      * @return int
      */
-    public function getStatus();
+    public function getStatus(): int;
 
     /**
      * @return array
      */
-    public function statusesList();
-
+    public function statusesList(): array;
 
 }
--- src/common/traits/ModelStatusTrait.php
+++ src/common/traits/ModelStatusTrait.php
@@ -13,28 +13,31 @@ trait ModelStatusTrait
     /**
      * @return int
      */
-    public function getStatus()
+    public function getStatus(): int
     {
-        return $this->status;
+        return (int) $this->status;
     }
 
     /**
      * Returns model statuses list array
      * @return array
      */
-    public function statusesList()
+    public function statusesList(): array
     {
         return [
             ModelStatus::STATUS_ACTIVE => Yii::t('yiicom', 'Active'),
             ModelStatus::STATUS_INACTIVE => Yii::t('yiicom', 'Inactive'),
             ModelStatus::STATUS_SOFT_DELETE => Yii::t('yiicom', 'Deleted'),
+            ModelStatus::STATUS_PROCESS => Yii::t('yiicom', 'Process'),
+            ModelStatus::STATUS_COMPLETE => Yii::t('yiicom', 'Complete'),
+            ModelStatus::STATUS_ERROR => Yii::t('yiicom', 'Error'),
         ];
     }
 
     /**
      * @return array
      */
-    public function statusesOptions()
+    public function statusesOptions(): array
     {
         return array_keys($this->statusesList());
     }
